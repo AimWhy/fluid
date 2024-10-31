6b026fcd29d223b12d42a9235699c8c21bffeeb2
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2024-10-31 06:49:22 +0100

    chore: fix build

--- src/imports/controls/controlsplugin.h
+++ src/imports/controls/controlsplugin.h
@@ -21,6 +21,6 @@ class FluidPlugin : public QQmlExtensionPlugin
     Q_OBJECT
     Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
 public:
-    void initializeEngine(QQmlEngine *engine, const char *uri);
-    void registerTypes(const char *uri);
+    void initializeEngine(QQmlEngine *engine, const char *uri) override;
+    void registerTypes(const char *uri) override;
 };
